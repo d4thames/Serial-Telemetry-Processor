dc8964cc3788e547c2dd05ccdce0f4b291b913d8
Harry Beadle <harry.beadle@gmail.com>
2017-03-03 12:30:03 +0000

    Code compiles

--- telemetry.c
+++ telemetry.c
@@ -13,19 +13,30 @@ UART1 Serial from PPM Decoder (RC Data)
 SPI   SPI from the IMU
 	MOSI Data from IMU
 	SCK  Clock from IMU
-	~SS  Pull Low
-
+	~SS  Pull LowUSART0_Rx_vect
+USART0_Rx_vectUSART0_Rx_vect
 ** Outputs
 UART0 Serial to Bluetooth Module
 	TODO
 
 */
 
+#include <stdint.h>
+//#include "/usr/avr/include/avr/iom644pa.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "buffer.h"
+
+
+#define DDR_SPI DDRB
+#define DD_MISO DDB5
+
+
 buffer8 b;
 
-ISR(USART0_Rx_vect)
+ISR(USART0_RX_vect)
 {
-	// RC Data Rx, put it in the buffer.
+	// RC Data Rx,S put it in the buffer.
 	buffer8_add(b, UDR0);
 }
 
@@ -46,23 +57,23 @@ void init_SPI(void)
 void init_UART0(unsigned int baud)
 {
 	// Set Baud Rate
-	UBRRH0 = (unsigned char) baud >> 8;
-	UBBRL0 = (unsigned char) baud;
+	UBRR0H = (unsigned char) baud >> 8;
+	UBRR0L = (unsigned char) baud;
 	// Enable Tx and Rx
-	UCSR0B = _BV(RXEN0) | _BV(TXEN0)
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0);
 	// Set frame: 8 Data, 2 Stop
-	UCSR0C = _BV(USBS0) | (3 << UCSZ00])
+	UCSR0C = _BV(USBS0) | (3 << UCSZ00);
 }
 
 void init_UART1(unsigned int baud)
 {
 	// Set Baud Rate
-	UBRRH1 = (unsigned char) baud >> 8;
-	UBBRL1 = (unsigned char) baud;
+	UBRR1H = (unsigned char) baud >> 8;
+	UBRR1L = (unsigned char) baud;
 	// Enable Tx and Rx
-	UCSR1B = _BV(RXEN1) | _BV(TXEN1)
+	UCSR1B = _BV(RXEN1) | _BV(TXEN1);
 	// Set frame: 8 Data, 2 Stop
-	UCSR1C = _BV(USBS1) | (3 << UCSZ10)
+	UCSR1C = _BV(USBS1) | (3 << UCSZ10);
 }
 
 int main(void)
@@ -79,7 +90,7 @@ int main(void)
 	while (1) {
 		// If the buffer is empty and the UART is not busy then
 		// output the next item from the buffer though UART0.
-		if (buffer8_rdy(b) && UARTREADY)
+		if (buffer8_rdy(b) && (UCSR0A & _BV(RXC0)))
 		{
 			UDR0 = buffer8_pop(b);
 		}
